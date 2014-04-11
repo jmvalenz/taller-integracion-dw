ec99088fdf67c1ad739c0262457595d80452044f
José Valenzuela Demarco <jmvalenz@wificlient-10-201-83-242.uc.cl>
2014-04-11 16:29:51 -0300

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DataWarehouse::Application.config.secret_key_base = 'a5ac98578a2fcf1f2077b68e3da9b8560fb3201b6db6bc0545bf23ba7433e544290976315e66c7f30e7f7375ea5dfb374b0a66adcbcde7dcab4c10d4f71e526c'
